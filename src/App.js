4a6ffe6f163c755e8959e88883e5312f89907015
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-10-12 14:34:50 -0400

    implemented chatbot on screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Typewriter from 'typewriter-effect';
+import Chat from './chat.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    chatStatus: false
+  }
+  handleClick = () => {
+    this.setState({chatStatus: true})
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Typewriter
+            options={{
+              strings: ['PRACTICE MAKES PERFECT'],
+              autoStart: true,
+              loop: true
+            }}
+            />
+          <p>Normalizing the negotiotion salary talk</p>
+          <button onClick={this.handleClick}>Start Practicing</button>
+        </header>
+        <Chat />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/chat.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import ChatBot from 'react-simple-chatbot';
+
+const arrayCompanies = ["PowerBitches", "Wager", "Quartet", "Dress For Success", "81cents", "Flatiron"]
+
+const rand = arrayCompanies[Math.floor(Math.random() * arrayCompanies.length)]
+
+const steps = [
+  {
+    id: '0',
+    message: '💸 Welcome to Practice Makes Paycheck! 💸',
+    trigger: '1',
+  },
+  {
+    id: '1',
+    message: 'What is your name? ',
+    trigger: '2'
+  },
+  {
+    id: '2',
+    user: true,
+    trigger: '3'
+  },
+  {
+    id: '3',
+    message: 'Hi {previousValue}, nice to meet you! What would you like to do today?',
+    trigger: '4'
+  },
+  {
+    id: '4',
+    options: [
+    { value: 1, label: 'Offer w/ Recruiter', trigger: '5' },
+    { value: 2, label: 'Negotioting', trigger: '5' },
+    { value: 3, label: 'Mock Interview', trigger: '5' },
+    { value: 4, label: 'Salary Expectations', trigger: '14' },
+  ]
+  },
+  {
+    id: '5',
+    message: 'Okay, lets start!',
+    trigger: '6'
+  },
+  {
+    id: '6',
+    message: 'Thank you for joining us today. Is now still a good time?',
+    trigger: '7'
+  },
+  {
+    id: '7',
+    user: true,
+    trigger: '8'
+  },
+  {
+    id: '8',
+    message: `Great! We’re pleased to offer you a position with ${rand} as a Designer. Your offer will include $80k, $40k, and $5k`,
+    trigger: '10'
+  },
+  {
+    id: '10',
+    user: true,
+    trigger: '11'
+  },
+  {
+    id: '11',
+    message: "Your total compensation would be $80k",
+    trigger: '12'
+  },
+  {
+    id: '12',
+    message: "Any questions so far?",
+    trigger: '13'
+  },
+  {
+    id: "13",
+    user: true,
+    end: true
+  },
+  {
+    id: "14",
+    message: "Tell me what state you live in",
+    trigger: "15"
+  },
+  {
+    id: "15",
+    user: true,
+    trigger: "16"
+  },
+  {
+    id: "16",
+    message: "In {previousValue}, the average salary is $61,372 as in 2017."
+  }
+];
+
+class Chat extends Component {
+  render() {
+    return (
+      <div>
+      <ChatBot steps={steps} />
+      </div>
+    )
+  }
+}
+
+export default Chat;
+
+// floating
